cc678ea1cbfd07d28f8b507b64d8d2c8c986ddc0
rvkrasyl <rvkrasyl@gmail.com>
2022-11-03 18:59:59 +0200

    done todo event added

--- src/components/TodoListItem/TodoListItem.js
+++ src/components/TodoListItem/TodoListItem.js
@@ -3,17 +3,33 @@ import React, { Component } from "react";
 import './TodoListItem.css';
 
 export default class TodoListItem extends Component {
-    render() {
+    state = {
+        done: false,
+    };
+
+    onLabelClick = () => {
+        this.setState({
+            done: !this.state.done,
+        });
+    };
     
+    render() {
+        
+        let classNames = 'todo-list-item';
+        if (this.state.done) {
+            classNames += ' done';
+        };
+
         const style = {
             color: this.props.important ? 'steelblue' : 'black',
             fontWeight: this.props.important ? 'bold' : 'normal',
-        } 
+        }; 
         
         return (
-            <span className="todo-list-item">
+            <span className={classNames}>
                 <span 
-                    className="todo-list-item-label" 
+                    className="todo-list-item-label"
+                    onClick={ this.onLabelClick } 
                     style={style}>
                     { this.props.label }
                 </span>
