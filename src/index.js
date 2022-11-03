c08cb1669bdea9e4e28a8bfb6b9ee2e4773808db
rvkrasyl <rvkrasyl@gmail.com>
2022-11-03 14:17:43 +0200

    structure updated

--- src/index.js
+++ src/index.js
@@ -1,31 +1,5 @@
-import React from 'react';
 import { createRoot } from 'react-dom/client';
 
-import AppHeader from './components/AppHeader';
-import SearchPanel from './components/SearchPanel';
-import TodoList from './components/TodoList';
-import ItemStatusFilter from './components/ItemStatusFilter';
-
-import './index.css';
-
-const App = () => {
-
-    const todoData = [
-        { id: 1, label: "Drink Coffee", important: false, },
-        { id: 2, label: "Create awesome React App", important: true, },
-        { id: 3, label: "Have a lunch", important: false, }
-    ];
-
-    return (
-        <div className='todo-app'>
-            <AppHeader toDo={1} done={3} />
-            <div className="top-panel d-flex">
-                <SearchPanel />
-                <ItemStatusFilter />
-            </div>
-            <TodoList todos={ todoData } />
-        </div>
-    );
-};
+import App from './components/App';
 
 createRoot(document.querySelector('#root')).render(<App />);
