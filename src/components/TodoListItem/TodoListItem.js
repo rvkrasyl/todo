c5e2612aa3f5450397a3f36f04b31cf8dc08dc37
rvkrasyl <rvkrasyl@gmail.com>
2022-11-03 18:24:21 +0200

    class-based components added

--- src/components/TodoListItem/TodoListItem.js
+++ src/components/TodoListItem/TodoListItem.js
@@ -1,34 +1,34 @@
-import React from "react";
+import React, { Component } from "react";
 
 import './TodoListItem.css';
 
-const TodoListItem = ( props ) => {
+export default class TodoListItem extends Component {
+    render() {
     
-    const style = {
-        color: props.important ? 'steelblue' : 'black',
-        fontWeight: props.important ? 'bold' : 'normal',
-    } 
+        const style = {
+            color: this.props.important ? 'steelblue' : 'black',
+            fontWeight: this.props.important ? 'bold' : 'normal',
+        } 
+        
+        return (
+            <span className="todo-list-item">
+                <span 
+                    className="todo-list-item-label" 
+                    style={style}>
+                    { this.props.label }
+                </span>
     
-    return (
-        <span className="todo-list-item">
-            <span 
-                className="todo-list-item-label" 
-                style={style}>
-                { props.label }
+                <button type="button"
+                        className="btn btn-putline-success btn-sm">
+                    <i className="fa fa-exclamation"/>
+                </button>
+    
+    
+                <button type="button"
+                        className="btn btn-putline-success btn-sm">
+                    <i className="fa fa-trash-o"/>
+                </button>
             </span>
-
-            <button type="button"
-                    className="btn btn-putline-success btn-sm">
-                <i className="fa fa-exclamation"/>
-            </button>
-
-
-            <button type="button"
-                    className="btn btn-putline-success btn-sm">
-                <i className="fa fa-trash-o"/>
-            </button>
-        </span>
-    );
-};
-
-export default TodoListItem;
+        );
+    }
+}
