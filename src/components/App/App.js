56e33fe03d7e9b099f3d3a04c60586507f79134b
rvkrasyl <rvkrasyl@gmail.com>
2022-11-04 13:42:17 +0200

    App class instead of App Func

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import AppHeader from '../AppHeader';
@@ -8,27 +8,29 @@ import ItemStatusFilter from '../ItemStatusFilter';
 
 import './App.css';
 
-const App = () => {
+class App extends Component {
+    render() {
 
-    const todoData = [
-        { id: 1, label: "Drink Coffee", important: false, },
-        { id: 2, label: "Create awesome React App", important: true, },
-        { id: 3, label: "Have a lunch", important: false, }
-    ];
-
-    return (
-        <div className='todo-app'>
-            <AppHeader toDo={1} done={3} />
-            <div className="top-panel d-flex">
-                <SearchPanel />
-                <ItemStatusFilter />
+        const todoData = [
+            { id: 1, label: "Drink Coffee", important: false, },
+            { id: 2, label: "Create awesome React App", important: true, },
+            { id: 3, label: "Have a lunch", important: false, }
+        ];
+    
+        return (
+            <div className='todo-app'>
+                <AppHeader toDo={1} done={3} />
+                <div className="top-panel d-flex">
+                    <SearchPanel />
+                    <ItemStatusFilter />
+                </div>
+                <TodoList 
+                todos={ todoData } 
+                onDeleted={ (id) => console.log(`${id} deleted`)}
+                />
             </div>
-            <TodoList 
-            todos={ todoData } 
-            onDeleted={ (id) => console.log(`${id} deleted`)}
-            />
-        </div>
-    );
-};
+        );
+    };
+}
 
 export default App;
